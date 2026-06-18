f328f2c6af7a1c7f1a732ce740d411abb49fb422
shamna <shamna@gmail.com>
2026-06-18 20:06:15 +0530

    Restore button styling for Start a Project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -209,7 +209,7 @@ export default function Home() {
             <Link to="/projects" className="btn btn-primary">
               <span>View Our Work</span>
             </Link>
-            <Link to="/contact" className="btn-arrow">
+            <Link to="/contact" className="btn">
               <span>Start a Project</span>
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                 <path d="M5 12h14M12 5l7 7-7 7" />
